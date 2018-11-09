0981d12414adbe6dd0bca3c0cbdb51cc2a84f26f
Agile Tross <scott.jackson.swe@gmail.com>
2018-11-09 10:05:44 -0700

    Second Try

--- python/challenges/robotVacuum/rv/aglo.py
+++ python/challenges/robotVacuum/rv/aglo.py
@@ -1,145 +1,142 @@
+# Max number of swipes = (Dirties Spot / MaxClean * 1.0)
+
+
+
+
+
+
 class Pod:
 
-    def __init__(self, x, y, s):
-        self.total = 0
-        self.loc_x = x
-        self.loc_y = y
+    def __init__(self,x,y,s):
+        self.x = x
+        self.y = y
         self.spot = s
-
-    def collect(self, amt):
-        self.total += amt
-
-    def move(self, d, m):
-        if d == RobVac.HORIZONTAL:
-            self.loc_x += m
-        else:
-            self.loc_y += m
-
-        if self.spot == 0:
-            self.loc_x += 1
-        if self.spot == 1:
-            self.loc_y += 1
-        if self.spot == 2:
-            self.loc_x -= 1
-        if self.spot == 3:
-            self.loc_y -= 1
-
-        self.spot += 1
-        if self.spot > 3:
-            self.spot = 0
+        self.total = 0
 
 
-class RobVac:
-    LEFT = -1
-    RIGHT = 1
-    UP = -1
-    DOWN = 1
-    HORIZONTAL = 0
-    VERTICAL = 1
+class State:
 
-    def __init__(self, max_collect):
-        self.pods=[Pod(0, 0, 0), Pod(1, 0, 1), Pod(1, 1, 2), Pod(0, 1, 3)]
-        self.max_collect = max_collect
-        self.last_move = self.HORIZONTAL
-        self.direction = [1, 1]
+    def __init__(self,room, max_clean):
+        self.pods = [Pod(0,0,1), Pod(1,0,1), Pod(1,1,1), Pod(0,1,3)]
+        self.cleaned = 0
+        self.room = room
+        self.max_clean = max_clean
+        self.upper_right_index = 0
+        self.direction_x = 1
+        self.direction_y = 1
 
-    def get_pod_stats(self):
-        ret_val = []
+
+
+    def result(self):
+        return [self.pods[0].total, self.pods[1].total, self.pods[2].total, self.pods[3].total]
+
+    def clean(self):
         for pod in self.pods:
-            ret_val.append(pod.total)
-        return ret_val
-
-    def perform_pod_collect(self, the_pod, board):
-        ret_val = False
-        if board[the_pod.loc_y][the_pod.loc_x] > self.max_collect:
-            # Clean max dirt, someone else will have to clean the rest
-            board[the_pod.loc_y][the_pod.loc_x] -= self.max_collect
-            the_pod.collect(self.max_collect)
-        elif board[the_pod.loc_y][the_pod.loc_x] == 0:
-            # Found uncounted clean square
-            ret_val = True
-            board[the_pod.loc_y][the_pod.loc_x] = -1
-        elif board[the_pod.loc_y][the_pod.loc_x] > 0:
-            # We are about to clean a square!
-            the_pod.collect(board[the_pod.loc_y][the_pod.loc_x])
-            ret_val = True
-            board[the_pod.loc_y][the_pod.loc_x] = -1
-
-        return ret_val
+            if self.room[pod.y][pod.x] == 0:
+                self.cleaned += 1
+                self.room[pod.y][pod.x] = -1
+            elif self.room[pod.y][pod.x] <= self.max_clean and self.room[pod.y][pod.x] > 0:
+                self.cleaned += 1
+                pod.total += self.room[pod.y][pod.x]
+                self.room[pod.y][pod.x] = -1
+            elif self.room[pod.y][pod.x] != -1:
+                self.room[pod.y][pod.x] -= self.max_clean
+                pod.total += self.max_clean
 
 
-    def find_pod(self,spot):
-        ret_val = -1
+    def rotate(self):
+
+      tmp_x = self.pods[0].x
+      tmp_y = self.pods[0].y
+
+      for i, pod in enumerate(self.pods):
+        if i < 3:
+          pod.x = self.pods[i+1].x
+          pod.y = self.pods[i + 1].y
+
+      self.pods[3].x = tmp_x
+      self.pods[3].y = tmp_y
+
+      if  self.upper_right_index <= 0:
+         self.upper_right_index = 3
+      else:
+         self.upper_right_index -= 1
+
+
+    def move(self):
+        change = False
+        x_move = 0
+        y_move = 0
+
+        # Determine if horizontal direction is flipping
+        if (self.pods[self.upper_right_index].x == (len(self.room[0])-2) and self.direction_x>0):
+            self.direction_x = -1
+            change = True
+        elif (self.pods[self.upper_right_index].x == 0 and self.direction_x < 0):
+            self.direction_x = 1
+            change = True
+
+        if change:
+            # Determine if vertical direction is flipping
+            if (self.pods[self.upper_right_index].y == (len(self.room) - 2) and self.direction_y > 0):
+                self.direction_y = -1
+            elif (self.pods[self.upper_right_index].y == 0 and self.direction_y < 0):
+                self.direction_y = 1
+
+            # Moving vertically
+            x_move = 0
+            y_move = self.direction_y
+        else: # Moving Horizontally
+            x_move = self.direction_x
+            y_move = 0
+
+
+        # Stash original location of Pod 0
+        tmp_x = self.pods[0].x
+        tmp_y = self.pods[0].y
+
+        # Loop over Pods, cleaning, rotating and moving
         for i, pod in enumerate(self.pods):
-            if spot == pod.spot:
-                ret_val = i
-        return ret_val
 
-    def right_boarder_check(self,board):
-        i = self.find_pod(1)
-        return self.pods[i].loc_x >= (len(board[0]) - 1)
+            # Clean current location
+            if self.room[pod.y][pod.x] == 0:
+                self.cleaned += 1
+                self.room[pod.y][pod.x] = -1
+            elif self.room[pod.y][pod.x] <= self.max_clean and self.room[pod.y][pod.x] > 0:
+                self.cleaned += 1
+                pod.total += self.room[pod.y][pod.x]
+                self.room[pod.y][pod.x] = -1
+            elif self.room[pod.y][pod.x] != -1:
+                self.room[pod.y][pod.x] -= self.max_clean
+                pod.total += self.max_clean
 
-    def left_boarder_check(self,board):
-        i = self.find_pod(0)
-        return self.pods[i].loc_x <= 0
+            # Rotate and move first 3 Pods
+            if i < 3:
+                pod.x = self.pods[i + 1].x
+                pod.y = self.pods[i + 1].y
+                pod.x += x_move
+                pod.y += y_move
 
-    def top_boarder_check(self,board):
-        i = self.find_pod(0)
-        return self.pods[i].loc_y <= 0
+        # Set Last Pod to location of original location of first pod and then rotate
+        self.pods[3].x = tmp_x + x_move
+        self.pods[3].y = tmp_y + y_move
 
-    def bot_boarder_check(self,board):
-        i = self.find_pod(2)
-        return self.pods[i].loc_y >= (len(board) - 1)
-
-    def next_dir(self, board):
-        ret_val = 0
-        if self.last_move == RobVac.HORIZONTAL:
-            if not self.left_boarder_check(board) and not self.right_boarder_check(board):
-                # Can move in either horizontal direction, so continue
-                ret_val = self.direction[0]
-            elif (self.left_boarder_check(board) and self.direction[0] == RobVac.RIGHT ) or\
-                (self.right_boarder_check(board) and self.direction[0] == RobVac.LEFT):
-                ret_val = self.direction[0]
-            elif (self.left_boarder_check(board) or self.right_boarder_check(board)):
-
-                # Reached horizontal boarder,
-                self.direction[0] = self.direction[0] * -1
-
-                if (self.top_boarder_check(board) and self.direction[1] == RobVac.UP) or\
-                   (self.bot_boarder_check(board) and self.direction[1] == RobVac.DOWN):
-                    # Reached horizontal boarder, but need to reverse vertical and horizontal
-                    self.direction[1] = self.direction[1] * -1
-
-                self.last_move = RobVac.VERTICAL
-                ret_val = self.direction[1]
+        # Update to the new upper right
+        if self.upper_right_index <= 0:
+            self.upper_right_index = 3
         else:
-            # Last move was vertical, so now move horizontal
-            self.last_move = RobVac.HORIZONTAL
-            ret_val = self.direction[0]
+            self.upper_right_index -= 1
 
-        return ret_val
 
-    def move(self, board):
-        ret_val = 0
-        d = self.next_dir(board)
-        for the_pod in self.pods:
-            if self.perform_pod_collect(the_pod, board):
-                ret_val += 1
-            the_pod.move(self.last_move, d)
-
-        return ret_val
 
 
 def robotVacuum(room, maxDirt):
+    squares = len(room) * len(room[0])
+    state = State(room, maxDirt)
 
-    bot = RobVac(maxDirt)
-    squares = len(room[0]) * len(room)
-    cleaned = 0
+    while state.cleaned < squares:
+        state.move()
+        print(room)
 
-    # Loop until board is clean
-    while cleaned != squares:
-        cleaned+=bot.move(room)
-
-    return bot.get_pod_stats()
-
+    return state.result()
 
--- python/challenges/robotVacuum/test/test_algo.py
+++ python/challenges/robotVacuum/test/test_algo.py
@@ -1,287 +1,127 @@
-from rv.aglo import Pod,RobVac,robotVacuum
+from rv.aglo import Pod,robotVacuum,State
+import pytest
+
+class TestAglo:
+
+    def disp_pods(self, state):
+
+        for i in range(len(state.pods)):
+            print("POD {}: {} {}".format(i, state.pods[i].x, state.pods[i].y))
 
 
-class TestAlgo:
+    def test_move_1(self):
+        room = [[1, 8, 5, 4],
+                [3, 4, 1, 0],
+                [3, 5, 2, 1],
+                [0, 2, 1, 4]]
+        maxDirt = 3
 
-    def test_pod_construct(self):
-        a = Pod(0, 0, 0)
-        a = Pod(0, 1, 1)
-        a = Pod(1, 0, 2)
-        a = Pod(1, 1, 3)
+        state = State(room, maxDirt)
 
+        # Initial State
+        assert state.pods[0].x == 0 and state.pods[0].y == 0
+        assert state.pods[1].x == 1 and state.pods[1].y == 0
+        assert state.pods[2].x == 1 and state.pods[2].y == 1
+        assert state.pods[3].x == 0 and state.pods[3].y == 1
 
-    def test_pod_collect(self):
-        a = Pod(0, 0, 0)
-        assert a.total == 0
-        a.collect(1)
-        assert a.total == 1
-        a.collect(5)
-        assert a.total == 6
+        self.disp_pods(state)
 
-    def test_pod_move(self):
-        a = Pod(0, 0, 0)
-        assert a.loc_x == 0 and a.loc_y == 0 and a.spot == 0
-
-        a.move(RobVac.HORIZONTAL,1)
-        assert a.loc_x == 2 and a.loc_y == 0 and a.spot == 1
-
-        a.move(RobVac.HORIZONTAL,1)
-        assert a.loc_x == 3 and a.loc_y == 1 and a.spot == 2
-
-        a.move(RobVac.HORIZONTAL, -1)
-        assert a.loc_x == 1 and a.loc_y == 1 and a.spot == 3
-
-        a.move(RobVac.VERTICAL, 1)
-        assert a.loc_x == 1 and a.loc_y == 1 and a.spot == 0
-
-        a.move(RobVac.VERTICAL, 1)
-        assert a.loc_x == 2 and a.loc_y == 2 and a.spot == 1
-
-        a.move(RobVac.VERTICAL, -1)
-        assert a.loc_x == 2 and a.loc_y == 2 and a.spot == 2
-
-
-    def test_RobVac_create(self):
-        a = RobVac(5)
-        assert a.max_collect == 5
-
-
-    def test_get_pod_stats(self):
-        a = RobVac(5)
-        assert a.get_pod_stats() == [0,0,0,0]
+        state.move()
 
 
 
-    def test_right_boarder_check(self):
-        a = RobVac(5)
+        # cleaned, Rotated,  and moved
+        assert [[-1, 5, 5, 4], [-1, 1, 1, 0], [3, 5, 2, 1], [0, 2, 1, 4]] == room
+        assert state.cleaned == 2
 
-        board=  [[0, 5, 4,10, 0, 8],
-        [7, 2, 7, 4, 7, 2],
-        [2, 2,10, 1, 7, 6]]
+        self.disp_pods(state)
 
-        assert not a.right_boarder_check(board)
-        a.pods[1].loc_x = 1
-        assert a.pods[1].loc_x == 1
-        assert not a.right_boarder_check(board)
-        a.pods[1].loc_x = 4
-        assert a.pods[1].loc_x == 4
-        assert not a.right_boarder_check(board)
+        assert state.pods[3].x == 1 and state.pods[3].y == 0
+        assert state.pods[0].x == 2 and state.pods[0].y == 0
+        assert state.pods[1].x == 2 and state.pods[1].y == 1
+        assert state.pods[2].x == 1 and state.pods[2].y == 1
 
-        a.pods[1].loc_x = 5
-        assert a.pods[1].loc_x == 5
-        assert a.right_boarder_check(board)
 
-        a.pods[1].loc_x = 6
-        assert a.pods[1].loc_x == 6
-        assert a.right_boarder_check(board)
+        state.move()
 
-    def test_left_boarder_check(self):
-        a = RobVac(5)
+        assert state.pods[2].x == 2 and state.pods[2].y == 0
+        assert state.pods[3].x == 3 and state.pods[3].y == 0
+        assert state.pods[0].x == 3 and state.pods[0].y == 1
+        assert state.pods[1].x == 2 and state.pods[1].y == 1
+
+        state.move()
+
+        assert state.pods[1].x == 2 and state.pods[1].y == 1
+        assert state.pods[2].x == 3 and state.pods[2].y == 1
+        assert state.pods[3].x == 3 and state.pods[3].y == 2
+        assert state.pods[0].x == 2 and state.pods[0].y == 2
+
+        state.move()
+
+        assert state.pods[0].x == 1 and state.pods[0].y == 1
+        assert state.pods[1].x == 2 and state.pods[1].y == 1
+        assert state.pods[2].x == 2 and state.pods[2].y == 2
+        assert state.pods[3].x == 1 and state.pods[3].y == 2
+
+        state.move()
+
+        assert state.pods[3].x == 0 and state.pods[3].y == 1
+        assert state.pods[0].x == 1 and state.pods[0].y == 1
+        assert state.pods[1].x == 1 and state.pods[1].y == 2
+        assert state.pods[2].x == 0 and state.pods[2].y == 2
+
+        state.move()
+
+        assert state.pods[2].x == 0 and state.pods[2].y == 2
+        assert state.pods[3].x == 1 and state.pods[3].y == 2
+        assert state.pods[0].x == 1 and state.pods[0].y == 3
+        assert state.pods[1].x == 0 and state.pods[1].y == 3
+
+        state.move()
+
+        assert state.pods[1].x == 1 and state.pods[1].y == 2
+        assert state.pods[2].x == 2 and state.pods[2].y == 2
+        assert state.pods[3].x == 2 and state.pods[3].y == 3
+        assert state.pods[0].x == 1 and state.pods[0].y == 3
+
+
+        state.move()
+
+        assert state.pods[0].x == 2 and state.pods[0].y == 2
+        assert state.pods[1].x == 3 and state.pods[1].y == 2
+        assert state.pods[2].x == 3 and state.pods[2].y == 3
+        assert state.pods[3].x == 2 and state.pods[3].y == 3
+
+        state.move()
+
+        assert state.pods[3].x == 2 and state.pods[3].y == 1
+        assert state.pods[0].x == 3 and state.pods[0].y == 1
+        assert state.pods[1].x == 3 and state.pods[1].y == 2
+        assert state.pods[2].x == 2 and state.pods[2].y == 2
+
+
+        state.move()
+        assert state.pods[2].x == 1 and state.pods[2].y == 1
+        assert state.pods[3].x == 2 and state.pods[3].y == 1
+        assert state.pods[0].x == 2 and state.pods[0].y == 2
+        assert state.pods[1].x == 1 and state.pods[1].y == 2
+
+    def test_robotVacuum_1(self):
         board = [[0, 5, 4, 10, 0, 8],
                  [7, 2, 7, 4, 7, 2],
                  [2, 2, 10, 1, 7, 6]]
-
-        assert a.left_boarder_check(board)
-        a.pods[0].loc_x = 1
-        assert a.pods[0].loc_x == 1
-        assert not a.left_boarder_check(board)
-        a.pods[0].loc_x = 4
-        assert a.pods[0].loc_x == 4
-        assert not a.left_boarder_check(board)
-
-        a.pods[0].loc_x = 5
-        assert a.pods[0].loc_x == 5
-        assert not a.left_boarder_check(board)
-
-
-    def test_top_boarder_check(self):
-        a = RobVac(5)
-        board = [[0, 5, 4, 10, 0, 8],
-                 [7, 2, 7, 4, 7, 2],
-                 [2, 2, 10, 1, 7, 6]]
-
-
-        assert a.top_boarder_check(board)
-
-        a.pods[0].loc_y = 1
-        assert a.pods[0].loc_y == 1
-        assert not a.top_boarder_check(board)
-
-        a.pods[0].loc_y = -1
-        assert a.pods[0].loc_y == -1
-        assert a.top_boarder_check(board)
-
-        a.pods[0].loc_y = 4
-        assert a.pods[0].loc_y == 4
-        assert not a.top_boarder_check(board)
-
-        a.pods[0].loc_y = 5
-        assert a.pods[0].loc_y == 5
-        assert not a.top_boarder_check(board)
-
-    def test_bot_boarder_check(self):
-        a = RobVac(5)
-        board = [[0, 5, 4, 10, 0, 8],
-                 [7, 2, 7, 4, 7, 2],
-                 [2, 2, 10, 1, 7, 6]]
-
-        assert not a.bot_boarder_check(board)
-
-        a.pods[2].loc_y = 1
-        assert a.pods[2].loc_y == 1
-        assert not a.bot_boarder_check(board)
-
-        a.pods[2].loc_y = -1
-        assert a.pods[2].loc_y == -1
-        assert not a.bot_boarder_check(board)
-
-        a.pods[2].loc_y = 2
-        assert a.pods[2].loc_y == 2
-        assert a.bot_boarder_check(board)
-
-        a.pods[2].loc_y = 3
-        assert a.pods[2].loc_y == 3
-        assert a.bot_boarder_check(board)
-
-
-    def test_rv_next_dir(self):
-        a = RobVac(5)
-        board = [[0, 5, 4, 10, 0, 8],
-                 [7, 2, 7, 4, 7, 2],
-                 [2, 2, 10, 1, 7, 6]]
-
-        print ("1st MOVE - Should be Horizontal to RIGHT")
-        assert a.last_move == RobVac.HORIZONTAL
-        assert not a.right_boarder_check(board)
-        assert RobVac.RIGHT == a.next_dir(board)
-        assert a.last_move == RobVac.HORIZONTAL
-
-        print("2nd MOVE - Should be Horizontal to RIGHT")
-        a.pods[0].loc_x = 1
-        assert not a.right_boarder_check(board)
-        assert RobVac.RIGHT == a.next_dir(board)
-        assert a.last_move == RobVac.HORIZONTAL
-
-        print("3rd Move - Should be Vertical Down")
-        a.pods[1].loc_x = 5
-        assert a.right_boarder_check(board)
-        assert RobVac.DOWN == a.next_dir(board)
-        assert a.last_move == RobVac.VERTICAL
-
-        print("Third MOVE")
-        assert RobVac.LEFT == a.next_dir(board)
-        assert a.last_move == RobVac.HORIZONTAL
-
-        print("Fourth MOVE")
-        a.pods[0].loc_x = 4
-        a.pods[0].loc_y = 2
-        a.pods[1].loc_x = 5
-        a.pods[1].loc_2 = 2
-        a.pods[2].loc_y = 3
-        a.pods[2].loc_x = 5
-        a.direction[0] = RobVac.RIGHT
-        assert a.bot_boarder_check(board)
-        assert a.right_boarder_check(board)
-        assert RobVac.UP == a.next_dir(board)
-        assert a.last_move == RobVac.VERTICAL
-
-    def test_rv_move(self):
-        a = RobVac(5)
-        board = [[0, 5, 4, 10, 0, 8],
-                 [7, 2, 7, 4, 7, 2],
-                 [2, 2, 10, 1, 7, 6]]
-
-        assert a.pods[0].loc_x == 0
-        assert a.pods[0].loc_y == 0
-        assert a.pods[0].spot == 0
-
-        assert a.pods[1].loc_x == 1
-        assert a.pods[1].loc_y == 0
-        assert a.pods[1].spot == 1
-
-        assert a.pods[2].loc_x  == 1
-        assert a.pods[2].loc_y == 1
-        assert a.pods[2].spot == 2
-
-        assert a.pods[3].loc_x  == 0
-        assert a.pods[3].loc_y == 1
-        assert a.pods[3].spot == 3
-
-        a.move(board)
-
-        assert a.pods[0].loc_x == 2
-        assert a.pods[0].loc_y == 0
-        assert a.pods[0].spot == 1
-
-        assert a.pods[1].loc_x == 2
-        assert a.pods[1].loc_y == 1
-        assert a.pods[1].spot == 2
-
-        assert a.pods[2].loc_x  == 1
-        assert a.pods[2].loc_y == 1
-        assert a.pods[2].spot == 3
-
-        assert a.pods[3].loc_x  == 1
-        assert a.pods[3].loc_y == 0
-        assert a.pods[3].spot == 0
-
-        print(board)
-        assert [0, 5, 2, 5] == a.get_pod_stats()
-        assert [[-1, -1, 4, 10, 0, 8],
-                 [2, -1, 7, 4, 7, 2],
-                 [2, 2, 10, 1, 7, 6]] == board
-
-
-
-    def test_perform_pod_collect(self):
-        board = [[0, 5, 4, 10, 0, 8],
-                 [7, 2, 7, 4, 7, 2],
-                 [2, 2, 10, 1, 7, 6]]
-
-        a = RobVac(5)
-        p = a.pods[0]
-
-        assert a.perform_pod_collect(p,board)
-        assert [0,0,0,0] == a.get_pod_stats()
-        assert board[0][0] == -1
-
-        p.loc_x = 1
-        assert a.perform_pod_collect(p, board)
-        print(board)
-        assert [5, 0, 0, 0] == a.get_pod_stats()
-        assert board[0][1] == -1
-
-        p.loc_x = 2
-        assert a.perform_pod_collect(p, board)
-        print(board)
-        assert [9, 0, 0, 0] == a.get_pod_stats()
-        assert board[0][2] == -1
-
-        p.loc_x = 3
-        assert not a.perform_pod_collect(p, board)
-        print(board)
-        assert [14, 0, 0, 0] == a.get_pod_stats()
-        assert board[0][3] == 5
-
-    def test_robotVacuum(self):
-        board = [[0, 5, 4, 10, 0, 8],
-                 [7, 2, 7, 4, 7, 2],
-                 [2, 2, 10, 1, 7, 6]]
-
         maxDirt = 10
-
-
         assert robotVacuum(board, maxDirt) == [18, 27, 13, 26]
 
+    def test_robotVacuum_2(self):
         room = [[1, 8, 5, 4],
-               [3, 4, 1, 0],
-               [3, 5, 2, 1],
-               [0, 2, 1, 4]]
-        maxDirt =  3
+                [3, 4, 1, 0],
+                [3, 5, 2, 1],
+                [0, 2, 1, 4]]
+        maxDirt = 3
         assert robotVacuum(room, maxDirt) == [8, 8, 13, 15]
 
-
-
+    def test_robotVacuum_3(self):
         room = [[0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0],
@@ -291,10 +131,13 @@ class TestAlgo:
         maxDirt = 3
         assert robotVacuum(room, maxDirt) == [0, 0, 0, 0]
 
+    def test_robotVacuum_4(self):
         room = [[5000 for column in range(3)] for row in range(3)]
         maxDirt = 1
         assert robotVacuum(room, maxDirt) == [20000, 10000, 5000, 10000]
 
+    #@pytest.mark.timeout(10)
+    def test_robotVacuum_5(self):
         room = [[0 for column in range(500)] for row in range(500)]
         maxDirt = 1
         assert robotVacuum(room, maxDirt) == [0, 0, 0, 0]
